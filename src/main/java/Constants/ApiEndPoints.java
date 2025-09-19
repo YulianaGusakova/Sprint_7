c2ee39962bfca9f3abe092c720187de0db4fbae6
Yuliana Gusakova <yulianagusakova@yandex.ru>
2025-09-19 21:39:43 +0300

    Создан CourierCreateTest

---
+++ src/main/java/Constants/ApiEndPoints.java
@@ -0,0 +1,7 @@
+package Constants;
+
+public class ApiEndPoints {
+        public static final String COURIER_CREATE_POST = "/api/v1/courier";
+        public static final String COURIER_LOGIN_POST = "/api/v1/courier/login";
+public static final String COURIER_ID_DELETE = "/api/v1/courier/{id}";
+}
---
+++ src/main/java/steps/CourierSteps.java
@@ -0,0 +1,34 @@
+package steps;
+
+import Constants.ApiEndPoints;
+import POJO.Courier;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+
+import static io.restassured.RestAssured.given;
+
+public class CourierSteps {
+    public ValidatableResponse createCourier(Courier courier) {
+       return given()
+                .body(courier)
+                .when()
+                .post(ApiEndPoints.COURIER_CREATE_POST)
+               .then();
+    }
+
+    public ValidatableResponse loginCourier(Courier courier) {
+        return given()
+                .body(courier)
+                .when()
+                .post(ApiEndPoints.COURIER_LOGIN_POST)
+                .then();
+    }
+
+    public ValidatableResponse deleteCourier(Courier courier) {
+        return given()
+                .pathParams("id", courier.getId())
+                .when()
+                .delete(ApiEndPoints.COURIER_ID_DELETE)
+                .then();
+    }
+}
